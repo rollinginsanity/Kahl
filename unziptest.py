a300538bb03dc2f03941c26ea4011ab4841375af
Reece <reece@rollinginsanity.net>
2015-09-20 22:06:28 +1000

    Testing the zip stuff with python

---
+++ unziptest.py
@@ -0,0 +1,7 @@
+#Just figuring out how to unzip files
+import zipfile
+
+zf = zipfile.ZipFile('comics/unprocessed/masseffect_foundation_vol1.cbz')
+for file in zf.namelist():
+    output = "comics/processed"
+    z.extract(file, output)
