f2b8b640bcda8900d1c021cfb11a25d607dd4c7f
Reece <reece@rollinginsanity.net>
2015-09-21 21:58:28 +1000

    Beginning e2e upload and extract process.

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,10 @@ from app import app
 import os
 from flask import request, redirect, url_for
 from werkzeug import secure_filename
+from redis import Redis
+from rq import Queue
+from addf import extractcomic
+import time
 
 UPLOAD_FOLDER = 'comics/unprocessed'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'cbz'])
@@ -11,6 +15,13 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+#Extract Comic Books into a directory
+def extractcomic(comicfile):
+	zf = zipfile.ZipFile(comicfile)
+	for file in zf.namelist():
+		output = "comics/processed/test2"
+		zf.extract(file, output)
 
 @app.route('/')
 @app.route('/index')
@@ -24,6 +35,7 @@ def upload():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            job = q.enqueue(extractcomic, app.config['UPLOAD_FOLDER']+filename)
             return redirect(url_for('index'))
     return '''
     <!doctype html>
