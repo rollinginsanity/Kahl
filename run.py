c00e45db8c7cb1eda00f611053000d309d64ffb2
rollinginsanity <reece.payne@gmail.com>
2015-12-28 12:11:24 +1100

    Now validates the structure of the config.ini file.

--- run.py
+++ run.py
@@ -30,9 +30,28 @@ if ("server" not in config.sections()):
     print("See https://github.com/rollinginsanity/Kahl for more info.")
     sys.exit()
 
-ServerBindIP = config[config_file_server_section]['ServerBindIP']
-DebugMode = giveMeBool(config[config_file_server_section]['DebugMode'])
-ListenPort = int(config[config_file_server_section]['ListenPort'])
+if ("ServerBindIP" in config[config_file_server_section]):
+    ServerBindIP = config[config_file_server_section]['ServerBindIP']
+else:
+    print("Missing ServerBindIP in the config file. Defaulting to 127.0.0.1")
+    ServerBindIP = "127.0.0.1"
+
+if ("DebugMode" in config[config_file_server_section]):
+    DebugMode = giveMeBool(config[config_file_server_section]['DebugMode'])
+else:
+    print("Missing DebugMode in the config file. Defaulting to False.")
+    DebugMode = False
+
+if ("ListenPort" in config[config_file_server_section]):
+    try:
+        ListenPort = int(config[config_file_server_section]['ListenPort'])
+    except Exception as e:
+        print("ListPort not set to a valid value. Needs to be an integer value.")
+        print("Defaulting to 5000")
+        ListenPort = 5000
+else:
+    print("Missing ListenPort in the config file. Defaulting to 5000")
+    ListenPort = 5000
 
 from app import app
 app.run(host=ServerBindIP, debug=DebugMode, port=ListenPort)
