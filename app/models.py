6969956dd05d309e9001362783c5b88df69ec0bd
rollinginsanity <reece.payne@gmail.com>
2015-12-28 14:55:54 +1100

    Added create, migate and downgrade scripts for the DB. Based on models.py

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db
 
 class Comic(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    cb_hash = db.Column(db.String(256), index=True, unique=True)
     title = db.Column(db.String(256), index=True, unique=True)
     author = db.Column(db.String(256), index=False, unique=False)
     genre = db.Column(db.String(256), index=False, unique=False)
@@ -9,3 +10,6 @@ class Comic(db.Model):
     franchise = db.Column(db.String(256), index=False, unique=False)
     issue_num = db.Column(db.Integer)
     volume_num = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<User %r>' % self.title
