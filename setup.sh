01893a18d24d10e0de1bcca9cbabb2ea37fcdff8
rollinginsanity <reece.payne@gmail.com>
2015-12-28 16:30:31 +1100

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
-mkdir app/static thumbs
-mkdir app/static/comics
+mkdir -p app/static/thumbs
+mkdir -p app/static/comics
+mkdir -p app/static/comics/processed
+mkdir -p app/static/comics/unprocessed
