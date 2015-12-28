1f43b8877f1e216e3c8de3dfcd18020df637d4d8
rollinginsanity <reece.payne@gmail.com>
2015-12-28 11:33:57 +1100

    Now has a config file. No error checking yet, will break if config is incorrect.

--- run.py
+++ run.py
@@ -1,2 +1,14 @@
+#Getting the configuration objects
+import configparser
+from misc_helpers import giveMeBool
+
+config = configparser.ConfigParser()
+
+config.read('config.ini')
+
+ServerBindIP = config['server']['ServerBindIP']
+DebugMode = giveMeBool(config['server']['DebugMode'])
+ListenPort = int(config['server']['ListenPort'])
+
 from app import app
-app.run(host='0.0.0.0', debug=True)
+app.run(host=ServerBindIP, debug=DebugMode, port=ListenPort)
