5a742a558d6d7d2bae8359b204e3d2ef28e5620a
rollinginsanity <reece.payne@gmail.com>
2016-05-25 22:31:36 +1000

    Changing function so something is running.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 nohup redis-server </dev/null >/dev/null 2>&1 & #
 nohup python3 /app/kahl/worker.py </dev/null >/dev/null 2>&1 & #
-nohup python3 /app/kahl/run.py </dev/null >/dev/null 2>&1 & #
+python3 /app/kahl/run.py
